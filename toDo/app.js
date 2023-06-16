850aebf0a82103d9214eaa53135b3631731bb84c
RufioYrael <4.3.1e.1987@gmail.com>
2023-06-15 21:05:04 -0400

    need to finish readme

--- toDo/app.js
+++ toDo/app.js
@@ -4,7 +4,9 @@ const toDoRoutes = require('./routes/toDoRoutes')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('combined'))
 app.use('/todos', toDoRoutes)
+
 
 module.exports = app
